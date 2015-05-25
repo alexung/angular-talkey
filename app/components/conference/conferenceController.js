6db8441a5e784b20a8b408967ee8064d0fddfd81
Alex Ung <alexung.ucsb@gmail.com>
2015-05-24 22:34:10 -0700

    Create dummy conference data

--- app/components/conference/conferenceController.js
+++ app/components/conference/conferenceController.js
@@ -1,4 +1,20 @@
 talkeyApp.controller('conferenceCtrl', function($scope) {
   $scope.message = "lol conference view";
+
+  $scope.conferences = [
+    {"eventName": "Alex's 23rd birthday",
+      "numOfAttendees": 9001,
+      "date": "today"
+    },
+    {"eventName": "Mike's birthday blast",
+      "numOfAttendees": 9002,
+      "date": "tomorrow"
+    },
+    {"eventName": "Napa Valley woohoo!",
+      "numOfAttendees": 2,
+      "date": "tomorrow"
+    }
+  ]
+
 });
 
