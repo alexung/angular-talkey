bde32e551baad2fd66ca1492b7cedc83f5868ce1
Alex Ung <alexung.ucsb@gmail.com>
2015-05-24 22:54:09 -0700

    Individual conference view is hooked up but param still shows up in url and can't interpolate values in the controller

--- app/components/conference/conferenceController.js
+++ app/components/conference/conferenceController.js
@@ -1,16 +1,24 @@
 talkeyApp.controller('conferenceCtrl', function($scope) {
+
+  var self = this;
+
+  self.lol = "lololololol";
+
   $scope.message = "lol conference view";
 
   $scope.conferences = [
-    {"eventName": "Alex's 23rd birthday",
+    {"conferenceId": 1,
+      "eventName": "Alex's 23rd birthday",
       "numOfAttendees": 9001,
       "date": "today"
     },
-    {"eventName": "Mike's birthday blast",
+    {"conferenceId": 2,
+      "eventName": "Mike's birthday blast",
       "numOfAttendees": 9002,
       "date": "tomorrow"
     },
-    {"eventName": "Napa Valley woohoo!",
+    {"conferenceId": 3,
+      "eventName": "Napa Valley woohoo!",
       "numOfAttendees": 2,
       "date": "tomorrow"
     }
