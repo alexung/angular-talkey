fcfb02a767820fc6c56c53eaa398c516ba4c0a73
Alex Ung <alexung.ucsb@gmail.com>
2015-05-29 14:14:26 -0700

    Fixed all the conferenceService issues by removing it

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -1,3 +1,5 @@
 talkeyApp.controller('homeCtrl', function($scope){
   $scope.message = "Test test TEST!!! Also Jessica Alba is very good looking.";
+
+  $scope.string1 = "String String string";
 });
