4fc94972f5cd8b2f2c936b0d6d678d1cfe41b8ee
Alex Ung <alexung.ucsb@gmail.com>
2015-05-29 16:32:10 -0700

    Instantiate localStorage in the app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,12 @@
-var talkeyApp = angular.module('talkeyApp', ['ngRoute']);
+var talkeyApp = angular.module('talkeyApp', ['ngRoute', 'LocalStorageModule']);
 // when you remove ngRoute from the dependency array, the message appears
+
+talkeyApp.config(function(localStorageServiceProvider){
+  localStorageServiceProvider
+    .setPrefix('talkeyApp')
+    .setStorageType('localStorage')
+    .setStorageCookie(0, '/')
+    .setStorageCookieDomain(window.location)
+    .setNotify(true, true)
+});
 
